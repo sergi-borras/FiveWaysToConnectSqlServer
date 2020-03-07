c937b76647c87f0de5f1a45f855aaa57b6feeee0
David Jimenez <djimenez92@outlook.es>
2020-03-07 20:41:36 +0100

    Create Table with Entity Framework CF close #4

---
+++ EntityFrameworkCodeFirst/Migrations/202003071931316_Create Student.cs
@@ -0,0 +1,28 @@
+﻿namespace EntityFrameworkCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateStudent : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Student",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Surname = c.String(),
+                        DateOfBirth = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Student");
+        }
+    }
+}
