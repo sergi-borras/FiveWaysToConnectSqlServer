d95b4dfece976a189ba7f7d0d580e0cf6bcc4e10
Sergi9610 <sborraspalcam@gmail.com>
2020-03-07 16:31:24 +0100

    Create Student model. #3

---
+++ StudentDAO/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDAO
+{
+    public class Student
+    {
+        public int Id;
+        public string Name;
+        public string Surname;
+        public DateTime DateOfBirth;
+
+        public Student(int id, string name, string surname, DateTime dateOfBirth)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            DateOfBirth = dateOfBirth;
+        }
+    }
+}
