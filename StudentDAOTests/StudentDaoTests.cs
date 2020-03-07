b8d9a1af86c6d31a08542168ef4443f7b7615714
Sergi9610 <sborraspalcam@gmail.com>
2020-03-07 21:39:05 +0100

    Added unit test for the Update functionality and some refactoring.

--- StudentDAOTests/StudentDaoTests.cs
+++ StudentDAOTests/StudentDaoTests.cs
@@ -11,20 +11,26 @@ namespace StudentDAO.Tests
     [TestClass()]
     public class StudentDaoTests
     {
-        public static Student student;
-        [TestInitialize]
-        public void SetUp()
-        {
-            student = new Student("Name1", "Surname1", new DateTime(2001, 01, 01));
-        }
         [TestMethod()]
         public void CreateTest()
         {
+            Student student = new Student("Name1", "Surname1", new DateTime(2001, 01, 01));
             StudentDao manager = new StudentDao();
-            int idToCheck = 1;
+            int idToCheck = 12;
             manager.Create(student);
             Student testStudent = manager.SelectStudentById(idToCheck);
             Assert.IsNotNull(testStudent);
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Student student = new Student("Update1", "Surname1", new DateTime(2001, 01, 01));
+            StudentDao manager = new StudentDao();
+            int idToCheck = 12;
+            manager.Update(student, idToCheck);
+            Student testStudent = manager.SelectStudentById(idToCheck);
+            Assert.AreEqual("Update1", testStudent.Name);
         }
     }
 }
