e51698772a049b0f92b1e7f73317eaf7c22ebe76
David Jimenez <djimenez92@outlook.es>
2020-03-08 19:10:07 +0100

    refactor student dao

--- EntityFrameworkCodeFirst/StudentDao.cs
+++ EntityFrameworkCodeFirst/StudentDao.cs
@@ -17,5 +17,37 @@ namespace EntityFrameworkCodeFirst
 			return student;
 		}
 
+		public List<Student> Read()
+		{
+			var students = context.Students.ToList();
+			return students;
+		}
+
+		public Student Update(Student student)
+		{
+			var studentToUpdate = context.Students.Find(student.Id);
+			if (studentToUpdate != null)
+			{
+				studentToUpdate.Name = student.Name;
+				studentToUpdate.Surname = student.Surname;
+				studentToUpdate.DateOfBirth = student.DateOfBirth;
+			}
+			context.SaveChanges();
+			return studentToUpdate;
+		}
+
+		public bool Delete(Student student)
+		{
+			var studentToDelete = context.Students.Find(student.Id);
+			if (studentToDelete != null)
+			{
+				context.Students.Remove(studentToDelete);
+				context.SaveChanges();
+			}
+			return (context.Students.Find(student.Id) == null);
+		}
+
+
+
 	}
 }
