f2981657e6333621c84ea36b2024b506dc5e57f2
Sergi9610 <sborraspalcam@gmail.com>
2020-03-08 15:58:15 +0100

    Create unit test for Create functionality.

---
+++ StoredProceduresTests/StudentDaoTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoredProcedures;
+using StudentDAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoredProcedures.Tests
+{
+    [TestClass()]
+    public class StudentDaoTests
+    {
+        public static StudentDao manager;
+        [TestInitialize]
+        public void Setup()
+        {
+            manager = new StudentDao();
+        }
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Student student = new Student("Create1", "Create1", new DateTime(2001, 01, 01));
+            manager.Create(student);
+            int idToCheck = 28;
+            Student testStudent = manager.SelectStudentById(idToCheck);
+            Assert.IsNotNull(testStudent);
+        }
+    }
+}
