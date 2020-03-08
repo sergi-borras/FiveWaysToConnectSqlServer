c1e936ca64bbf0c0594e9898577430aba4ce6951
David Jimenez <djimenez92@outlook.es>
2020-03-08 18:51:01 +0100

    Connect with database and create context close #18

---
+++ EntityFrameworkDatabaseFirst/VuelingDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace EntityFrameworkDatabaseFirst
+{
+    public class VuelingDbContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public VuelingDbContext() : base("name=VuelingContext")
+        {}
+       
+
+    }
+}
---
+++ EntityFrameworkDatabaseFirstTests/StudentDaoTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace EntityFrameworkDatabaseFirst.Tests
+{
+	[TestClass()]
+	public class StudentDaoTests
+	{
+
+		public static StudentDao studentDao;
+		public static Student student;
+		[TestMethod()]
+		public void CreateDefaultTest()
+		{
+			studentDao = new StudentDao();
+			student = new Student();
+			student.Name = "David"; student.Surname = "Jiménez";
+			student.DateOfBirth = new DateTime(1992, 6, 24);
+			var spected = studentDao.Create(student);
+			Assert.AreEqual(spected, student);
+		}
+	}
+		
+}
