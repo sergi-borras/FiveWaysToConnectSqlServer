2a24eaa33f26e3c5ddd1791e792cbaa0a49f3b24
Sergi9610 <sborraspalcam@gmail.com>
2020-03-08 15:57:38 +0100

    Create Student class.

---
+++ StoredProcedures/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDAO
+{
+    public class Student
+    {
+        public int Id;
+        public string Name;
+        public string Surname;
+        public DateTime Birthdate;
+
+        public Student(string name, string surname, DateTime birthDate)
+        {
+            Name = name;
+            Surname = surname;
+            Birthdate = birthDate;
+        }
+
+        public Student() { }
+    }
+}
+
