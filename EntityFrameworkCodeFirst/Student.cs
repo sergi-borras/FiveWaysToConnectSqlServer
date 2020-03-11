61f63dbf8c9749ffdaf76952434e4d9a17e0ddd1
David Jimenez <djimenez92@outlook.es>
2020-03-11 08:59:22 +0100

    Code first refactor

--- EntityFrameworkCodeFirst/Student.cs
+++ EntityFrameworkCodeFirst/Student.cs
@@ -13,9 +13,8 @@ namespace EntityFrameworkCodeFirst
 		public string Surname { get; set; }
 		public DateTime DateOfBirth { get; set; }
 
-		public Student(int id, string name, string surname, DateTime dateOfBirth)
+		public Student(string name, string surname, DateTime dateOfBirth)
 		{
-			Id = id;
 			Name = name;
 			Surname = surname;
 			DateOfBirth = dateOfBirth;
--- EntityFrameworkCodeFirst/StudentDao.cs
+++ EntityFrameworkCodeFirst/StudentDao.cs
@@ -1,51 +1,126 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System;
+using System.Data.Entity.Validation;
+using System.Data.Entity.Infrastructure;
 
 namespace EntityFrameworkCodeFirst
 {
 	public class StudentDao
 	{
-		private readonly VuelingDbContext context = new VuelingDbContext();
+		private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(typeof(StudentDao));
 
 		public Student Create(Student student)
 		{
-
-			var studentInserted = context.Students.Add(student);
-			context.SaveChanges();
-			return studentInserted;
+			using (var context = new VuelingDbContext())
+			{
+				var studentInserted = context.Students.Add(student);
+				SaveInContext(context);
+				return studentInserted;
+			}
 		}
 
 		public List<Student> Read()
 		{
-			var students = context.Students.ToList();
-			return students;
+			try
+			{
+				using (var context = new VuelingDbContext())
+				{
+					var students = context.Students.ToList();
+					return students;
+				}
+			}
+			catch (ArgumentNullException ane)
+			{
+				logger.Error(Resources.Message + ane.Message);
+				logger.Error(Resources.Trace + ane.StackTrace);
+				throw;
+			}
 		}
 
 		public Student Update(Student student)
 		{
-			var studentToUpdate = context.Students.Find(student.Id);
-			if (studentToUpdate != null)
+			try
 			{
-				studentToUpdate.Name = student.Name;
-				studentToUpdate.Surname = student.Surname;
-				studentToUpdate.DateOfBirth = student.DateOfBirth;
+				using (var context = new VuelingDbContext())
+				{
+					var studentToUpdate = context.Students.Find(student.Id);
+					studentToUpdate.Name = student.Name;
+					studentToUpdate.Surname = student.Surname;
+					studentToUpdate.DateOfBirth = student.DateOfBirth;
+
+					SaveInContext(context);
+					return studentToUpdate;
+				}
 			}
-			context.SaveChanges();
-			return studentToUpdate;
+			catch (InvalidOperationException ioe)
+			{
+				logger.Error(Resources.Message + ioe.Message);
+				logger.Error(Resources.Trace + ioe.StackTrace);
+				throw;
+			}
+
 		}
 
 		public bool Delete(Student student)
 		{
-			var studentToDelete = context.Students.Find(student.Id);
-			if (studentToDelete != null)
+			try
 			{
-				context.Students.Remove(studentToDelete);
-				context.SaveChanges();
+				using (var context = new VuelingDbContext())
+				{
+					var studentToDelete = context.Students.Find(student.Id);
+					context.Students.Remove(studentToDelete);
+					SaveInContext(context);
+
+					return (context.Students.Find(student.Id) == null);
+				}
 			}
-			return (context.Students.Find(student.Id) == null);
+			catch (InvalidOperationException ioe)
+			{
+				logger.Error(Resources.Message + ioe.Message);
+				logger.Error(Resources.Trace + ioe.StackTrace);
+				throw;
+			}
 		}
 
+		public void SaveInContext(VuelingDbContext vContext)
+		{
+			try
+			{
+				vContext.SaveChanges();
+			}
+			catch (DbUpdateException due)
+			{
+				logger.Error(Resources.Message + due.Message);
+				logger.Error(Resources.Trace + due.StackTrace);
+				throw;
+			}
+			catch (DbEntityValidationException deve)
+			{
+				logger.Error(Resources.Message + deve.Message);
+				logger.Error(Resources.Trace + deve.StackTrace);
+				throw;
+			}
+			catch (NotSupportedException nse)
+			{
+				logger.Error(Resources.Message + nse.Message);
+				logger.Error(Resources.Trace + nse.StackTrace);
+				throw;
+			}
+			catch (ObjectDisposedException ode)
+			{
+				logger.Error(Resources.Message + ode.Message);
+				logger.Error(Resources.Trace + ode.StackTrace);
+				throw;
+			}
+			catch (InvalidOperationException ioe)
+			{
+				logger.Error(Resources.Message + ioe.Message);
+				logger.Error(Resources.Trace + ioe.StackTrace);
+				throw;
+			}
 
+		}
 
 	}
 }
